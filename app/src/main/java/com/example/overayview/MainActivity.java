a90b9da3e5964630d3601e312d99c54e84855d63
JongWanKim <46095454+oen142@users.noreply.github.com>
2019-08-21 14:31:48 +0900

    touchEvent
    Touch Abee App

--- app/src/main/java/com/example/overayview/MainActivity.java
+++ app/src/main/java/com/example/overayview/MainActivity.java
@@ -1,43 +1,90 @@
 package com.example.overayview;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String alert_abee_ad_st = "";
 
     private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
 
 
-        Button bt_start = (Button) findViewById(R.id.btn1);
-        bt_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkPermission();
-                //startService(new Intent(MainActivity.this, AlwaysOnTopService.class));
-            }
-        });
+        SharedPreferences sf = getSharedPreferences("abee_ad",MODE_PRIVATE);
+        alert_abee_ad_st = sf.getString("abee_ad","");
 
-        Button bt_stop = (Button) findViewById(R.id.btn2);
-        bt_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopService(new Intent(MainActivity.this, MyService.class));
-            }
-        });
+        alert_abee_ad(alert_abee_ad_st);
+
     }
 
+
+
+    public void  alert_abee_ad(String alert_abee_ad_st){
+
+
+        if(alert_abee_ad_st == "yes") {
+            startService(new Intent(MainActivity.this, MyService.class));
+        }else{
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    this);
+
+            alertDialogBuilder.setTitle("아비 광고설정");
+            alertDialogBuilder
+                    .setMessage("아비 광고를 보시겠습니까?")
+                    .setCancelable(false)
+                    .setPositiveButton("광고설정",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+
+                                    Toast.makeText(MainActivity.this , "아비광고 설정합니다." , Toast.LENGTH_LONG).show();
+                   //                 checkPermission();
+                                    startService(new Intent(getApplicationContext(), MyService.class));
+             /*                       SharedPreferences sharedPreferences = getSharedPreferences("abee_ad",MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("abee_ad" , "yes");
+                                    editor.commit();*/
+                                }
+                            })
+                    .setNegativeButton("취소",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        DialogInterface dialog, int id) {
+                                    Toast.makeText(MainActivity.this , "아비광고 설정을 하지 않습니다." , Toast.LENGTH_LONG).show();
+                                    dialog.cancel();
+                                }
+                            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // 다이얼로그 보여주기
+            alertDialog.show();
+
+        }
+
+    }
     public void checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
             if (!Settings.canDrawOverlays(this)) {              // 체크
@@ -51,18 +98,10 @@ public class MainActivity extends AppCompatActivity {
             startService(new Intent(MainActivity.this, MyService.class));
         }
     }
+    public void turnoff_abee_ad(View v){
 
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
-            if (!Settings.canDrawOverlays(this)) {
-                // TODO 동의를 얻지 못했을 경우의 처리
 
-            } else {
-                startService(new Intent(MainActivity.this, MyService.class));
-            }
-        }
+        stopService(new Intent(MainActivity.this, MyService.class));
     }
+
 }
--- app/src/main/java/com/example/overayview/MyService.java
+++ app/src/main/java/com/example/overayview/MyService.java
@@ -3,16 +3,17 @@ package com.example.overayview;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -22,8 +23,14 @@ public class MyService extends Service {
     WindowManager wm;
     View mView;
 
+    private float mTouchX, mTouchY;
+    private int mViewX, mViewY;
+    WindowManager.LayoutParams params;
+
     @Override
-    public IBinder onBind(Intent intent) { return null; }
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
     @Override
     public void onCreate() {
@@ -34,15 +41,17 @@ public class MyService extends Service {
         int LAYOUT_FLAG;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            System.out.println("타입앺");
         } else {
             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+            System.out.println("타입폰");
         }
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 LAYOUT_FLAG,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN  | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);/*
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 *//*ViewGroup.LayoutParams.MATCH_PARENT*//*300,
@@ -54,47 +63,63 @@ public class MyService extends Service {
                 PixelFormat.TRANSLUCENT);
 */
 
-        Random random = new Random();
-        int random_fosition = random.nextInt(4);
-
-        switch (random_fosition){
-
-            case 0 : params.gravity = Gravity.LEFT | Gravity.TOP;
-            break;
-            case 1 : params.gravity = Gravity.RIGHT | Gravity.TOP;
-                break;
-            case 2 : params.gravity = Gravity.LEFT | Gravity.BOTTOM;
-                break;
-            case 3 : params.gravity = Gravity.RIGHT | Gravity.BOTTOM;
-                break;
-
-        }
         mView = inflate.inflate(R.layout.view_in_service, null);
-  //      final TextView textView = (TextView) mView.findViewById(R.id.textView);
-        final ImageButton bt =  (ImageButton) mView.findViewById(R.id.bt);
-        bt.setOnClickListener(new View.OnClickListener() {
+        //      final TextView textView = (TextView) mView.findViewById(R.id.textView);
+
+        final LinearLayout viewservice_ll = mView.findViewById(R.id.viewservice_ll);
+
+
+        viewservice_ll.setOnTouchListener(new View.OnTouchListener() {
+
             @Override
-            public void onClick(View v) {
-                bt.setImageResource(R.mipmap.ic_launcher_round);
-          //      textView.setText("on click!!");
-                Toast.makeText(getApplicationContext() , "하이" , Toast.LENGTH_LONG).show();
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                System.out.println("터치이벤트");
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        mTouchX = motionEvent.getRawX();
+                        mTouchY = motionEvent.getRawY();
+                        mViewX = params.x;
+                        mViewY = params.y;
+
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        int x = (int) (motionEvent.getRawX() - mTouchX);
+                        int y = (int) (motionEvent.getRawY() - mTouchY);
+
+                        params.x = mViewX + x;
+                        params.y = mViewY + y;
+
+                        wm.updateViewLayout(mView, params);
+
+                        break;
+                }
+
+                return true;
             }
+
         });
+
+
+
         wm.addView(mView, params);
-
-
     }
-
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(wm != null) {
-            if(mView != null) {
+        if (wm != null) {
+            if (mView != null) {
                 wm.removeView(mView);
                 mView = null;
             }
             wm = null;
         }
     }
+
+
 }
