1c71bffd59d6f7227fb77cbd800bfa96855ace23
JongWanKim <46095454+oen142@users.noreply.github.com>
2019-08-21 17:19:48 +0900

    abeeadvertise

--- app/src/main/java/com/example/overayview/MyService.java
+++ app/src/main/java/com/example/overayview/MyService.java
@@ -3,20 +3,19 @@ package com.example.overayview;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.IBinder;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.util.Random;
 
 public class MyService extends Service {
 
@@ -26,7 +25,7 @@ public class MyService extends Service {
     private float mTouchX, mTouchY;
     private int mViewX, mViewY;
     WindowManager.LayoutParams params;
-
+    int x ,y ;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -41,10 +40,8 @@ public class MyService extends Service {
         int LAYOUT_FLAG;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-            System.out.println("타입앺");
         } else {
             LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
-            System.out.println("타입폰");
         }
 
         params = new WindowManager.LayoutParams(
@@ -52,28 +49,22 @@ public class MyService extends Service {
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 LAYOUT_FLAG,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN  | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);/*
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                *//*ViewGroup.LayoutParams.MATCH_PARENT*//*300,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                        |WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                        |WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
-*/
+
 
         mView = inflate.inflate(R.layout.view_in_service, null);
-        //      final TextView textView = (TextView) mView.findViewById(R.id.textView);
-
         final LinearLayout viewservice_ll = mView.findViewById(R.id.viewservice_ll);
-
-
         viewservice_ll.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 System.out.println("터치이벤트");
+                Display display = ((WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
+                int width = size.x;
+                int height = size.y;
+
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
 
@@ -88,24 +79,36 @@ public class MyService extends Service {
                         break;
 
                     case MotionEvent.ACTION_MOVE:
-                        int x = (int) (motionEvent.getRawX() - mTouchX);
-                        int y = (int) (motionEvent.getRawY() - mTouchY);
+                        x = (int) (motionEvent.getRawX() - mTouchX);
+                        y = (int) (motionEvent.getRawY() - mTouchY);
 
                         params.x = mViewX + x;
                         params.y = mViewY + y;
+                        Log.e("X" , String.valueOf(x) + "X좌표");
+                        Log.e("X" , String.valueOf(y) + "Y좌표");
+
+                        Log.e("X" , String.valueOf(params.x) + "mTouchX좌표");
+                        Log.e("X" , String.valueOf(params.y) + "mTouchY좌표");
+                        Log.e("X" , String.valueOf(width) + "displayX좌표");
+                        Log.e("X" , String.valueOf(height) + "displayY좌표");
+                        Log.e("X" , String.valueOf((-(height- height/2)+(height/7)) ) + "뭘까용");
 
                         wm.updateViewLayout(mView, params);
 
                         break;
                 }
 
+
+
+                if( (params.y < (-(height- height/2)+200) )&&((params.x >-30) && params.x < 30) ){
+                    Toast.makeText(getApplicationContext() , "오픈 url" , Toast.LENGTH_LONG).show();
+                    wm.removeView(mView);
+                }
+
                 return true;
             }
 
         });
-
-
-
         wm.addView(mView, params);
     }
 
@@ -120,6 +123,4 @@ public class MyService extends Service {
             wm = null;
         }
     }
-
-
 }
