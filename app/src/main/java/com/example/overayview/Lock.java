21aa00aca1bdee18e41de22435898deec0c2315d
JongWanKim <46095454+oen142@users.noreply.github.com>
2019-08-21 17:54:09 +0900

    lock

---
+++ app/src/main/java/com/example/overayview/Lock.java
@@ -0,0 +1,14 @@
+package com.example.overayview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+    }
+}
