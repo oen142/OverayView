38d769e1729290b9da5b55cd4297675252e7475f
dongu <nokr82@gmail.com>
2019-08-23 16:11:35 +0900

    cc

--- app/src/main/java/com/example/overayview/Splash.java
+++ app/src/main/java/com/example/overayview/Splash.java
@@ -24,7 +24,6 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run() {
 
-//                loading_Page();
 
                 Intent it = new Intent(getApplicationContext() , MainActivity.class);
                 startActivity(it);
@@ -33,21 +32,5 @@ public class Splash extends AppCompatActivity {
         }, 1000);
     }
 
-   /* public void loading_Page(){
-        if (mProgress == null) {
-            mProgress = new WebviewProgressDialog(getApplicationContext(),R.style.myDialog);
-            mProgress.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT)); // 배경 투명처리
-            //mProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            //mProgress.setTitle("Loading...");
-            //mProgress.setMessage("불러오는중...");
-            mProgress.setCancelable(false);
-	            *//*mProgress.setButton("Cancel", new DialogInterface.OnClickListener() {
-	                @Override
-	                public void onClick(DialogInterface dialog, int whichButton) {
-	                    mProgress.dismiss();
-	                }
-	            });*//*
-            mProgress.show();
-        }
-    }*/
+
 }
